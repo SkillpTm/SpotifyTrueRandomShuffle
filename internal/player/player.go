394a10ad0b4405b4b1124e1ee46c148d6fcdf63d
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-02-23 17:20:13 +0100

    added player struct and RunChecks

---
+++ internal/player/player.go
@@ -0,0 +1,30 @@
+// Package player ...
+package player
+
+// <---------------------------------------------------------------------------------------------------->
+
+
+
+type Player struct {
+	isPlaying bool
+	currentlyPlayingType string
+	repeatState string
+	shuffleState bool
+	smartShuffle bool
+	contextType string
+	contextHREF string
+}
+
+
+func (player *Player) RunChecks() bool {
+	if (!player.isPlaying || // check if the user is playing something right now
+		player.currentlyPlayingType != "track" || // check if the user is listening to a track
+		player.repeatState == "track" || // check if the user has repeat turned on for this track
+		!player.shuffleState || // check if the player has turned on shuffle
+		player.smartShuffle || // check if the user hasn't turned on smart shuffle
+		player.contextType == "show" || // check if the user's context isn't a show
+		player.contextType == "artist") { // check if the user's context isn't an artist
+		return true
+	}
+	return false
+}
