b1c00dd6e3334d8289a878b0c3b7401b5d91b5e8
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-02-23 15:59:44 +0100

    added Token class and a getter that ensures the token is valid

---
+++ internal/api/Token.go
@@ -0,0 +1,56 @@
+// Package api ...
+package api
+
+// <---------------------------------------------------------------------------------------------------->
+
+import (
+	"encoding/base64"
+	"time"
+
+	"github.com/SkillpTm/SpotifyTrueRandomShuffle/internal/util"
+)
+
+// <---------------------------------------------------------------------------------------------------->
+
+type Token struct {
+	AccessToken string
+	ExpirationTime time.Time
+	RefreshToken string
+}
+
+// <---------------------------------------------------------------------------------------------------->
+
+
+
+func (token *Token) GetAccessToken() string {
+	currentTime := time.Now()
+
+	if (token.ExpirationTime.Before(currentTime)) {
+		token.refreshAccessToken(currentTime)
+	}
+
+	return token.AccessToken
+}
+
+
+func (token *Token) refreshAccessToken(currentTime time.Time) error {
+    parameters := map[string]string{
+        "grant_type": "refresh_token",
+        "refresh_token" : token.RefreshToken,
+    }
+    headers := map[string]string{
+        "Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte(clientID+":"+clientSecret)),
+        "Content-Type" : "application/x-www-form-urlencoded",
+    }
+
+	responseMap, err := util.MakePostRequest("https://accounts.spotify.com/api/token", parameters, headers)
+	if err != nil {
+        return err
+    }
+
+	token.AccessToken = responseMap["access_token"].(string)
+	token.ExpirationTime = currentTime.Add(time.Hour)
+	token.RefreshToken = responseMap["refresh_token"].(string)
+
+	return nil
+}
