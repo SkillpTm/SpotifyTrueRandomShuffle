41909e6de53963f0d29fc2186c046dba3c38a381
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-02-26 21:08:38 +0100

    added util.Setup and function comment

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,13 +3,22 @@ package main
 // <---------------------------------------------------------------------------------------------------->
 
 import (
+	"log"
+
 	"github.com/SkillpTm/SpotifyTrueRandomShuffle/internal/api"
 	"github.com/SkillpTm/SpotifyTrueRandomShuffle/internal/player"
+	"github.com/SkillpTm/SpotifyTrueRandomShuffle/internal/util"
 )
 
 // <---------------------------------------------------------------------------------------------------->
 
+// main is the entry into the program, it sets up our config, envs, authorizes with the user and starts the main loop
 func main() {
+	err := util.Setup()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
     api.AuthUser()
     player.Start()
 }
