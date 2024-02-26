adaa14e125cc5ea30fe4b3f7511519f36c58796e
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-02-26 19:47:50 +0100

    change importConfig to set values to config var instead of returning it

--- internal/util/setup.go
+++ internal/util/setup.go
@@ -12,28 +12,36 @@ import (
 
 // <---------------------------------------------------------------------------------------------------->
 
+var Config map[string]interface{}
+
+// <---------------------------------------------------------------------------------------------------->
+
 // importConfig imports to config file from ./configs/config
-func importConfig() (map[string]interface{}, error) {
-    var configData map[string]interface{}
+func importConfig() (error) {
 
     // import config file
     configFile, err := os.Open("./configs/config.json")
     if err != nil {
-        return configData, errors.New("couldn't open config file: " + err.Error())
+        return errors.New("couldn't open config file: " + err.Error())
     }
     defer configFile.Close()
 
     // read config data from file
     rawConfigData, err := io.ReadAll(configFile)
     if err != nil {
-        return configData, errors.New("couldn't read config file: " + err.Error())
+        return errors.New("couldn't read config file: " + err.Error())
     }
+
+	var configData map[string]interface{}
 
     // convert data to map
     err = json.Unmarshal(rawConfigData, &configData)
     if err != nil {
-        return configData, errors.New("couldn't unmarshal raw config data: " + err.Error())
+        return errors.New("couldn't unmarshal raw config data: " + err.Error())
     }
 
-    return configData, nil
+	// set configData to exportable var
+	Config = configData
+
+    return nil
 }
